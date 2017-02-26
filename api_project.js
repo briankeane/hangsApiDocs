2cb7a37205eb70e48dd7ce84030adafc5f5e9f5f
briankeane <lonesomewhistle@gmail.com>
2017-02-26 13:26:00 -0600

    first version

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Hangs",
+  "version": "1.0.0",
+  "description": "Invite your friends to hangout without all the pressure and pain.",
+  "title": "Hangs Documentation",
+  "url": "https://www.hangsApp.com",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-02-26T19:23:50.941Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.3"
+  }
+});
