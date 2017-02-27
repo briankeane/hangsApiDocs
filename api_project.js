4c43fddad4e3f6fedbd4053f78d197934446b2b8
briankeane <lonesomewhistle@gmail.com>
2017-02-27 13:37:11 -0600

    added more endpoints

--- api_project.js
+++ api_project.js
@@ -4,11 +4,16 @@ define({
   "description": "Invite your friends to hangout without all the pressure and pain.",
   "title": "Hangs Documentation",
   "url": "https://www.hangsApp.com/api/v1",
+  "order": [
+    "Users",
+    "Hangouts",
+    "Groups"
+  ],
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-02-27T18:32:51.100Z",
+    "time": "2017-02-27T19:36:36.326Z",
     "url": "http://apidocjs.com",
     "version": "0.12.3"
   }
