0ccf4d4fb5f9c63e3d0f6d23e7f1fb7a1161477f
briankeane <lonesomewhistle@gmail.com>
2017-03-12 22:02:58 -0500

    added getMyHangouts and getMyInvites endpoints

--- api_project.js
+++ api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-03-02T22:23:04.586Z",
+    "time": "2017-03-13T03:02:38.616Z",
     "url": "http://apidocjs.com",
     "version": "0.12.3"
   }
